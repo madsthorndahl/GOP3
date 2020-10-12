a4b0ab66433706e3e8abbab5d0e2a3237a10cb62
Mads Thorndahl <madsthorndahl@Madss-MacBook-Pro.local>
2020-10-12 13:19:15 +0200

    first commit

---
+++ classes.js
@@ -0,0 +1,50 @@
+
+class User {
+
+    constructor(name, birthday, gender, location)
+        {
+      
+        this.name = name
+        this.birthday = birthday;
+        this.gender = gender;
+        this.location = location;
+    //Lav en like funiton
+        };
+
+class freeUser extends user{
+    
+            constructor(name, birthday, gender, location,){
+            super(name, birthday, gender, location)
+            //add like limit
+            
+        }}
+        
+
+class paymenUser extends user{
+    
+    constructor(name, birthday, gender, location,){
+    super(name, birthday, gender, location)
+    //add super like eller sådan noget
+    
+}}
+
+class creditCard {
+    constructor(cardHolderName, experationDate, cardNumber, CVCNumber)
+
+}
+
+class interst extends user {
+    constructor(name,)
+    super(name)
+    //hvis personen liker CRUD til en liste med alle folk som de liker
+
+}
+
+class match extends interst {
+    constructor(name)
+    super(name)
+    //metoder der sammenligner forskellige crud for forskellige persone og derved laver matches, og giver acces 
+    // til en message function
+
+}
+# GOP3
