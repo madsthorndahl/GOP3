202a8309f836045442a6399bc5a21bbea26a8b6c
Mads Thorndahl <madsthorndahl@Madss-MacBook-Pro.local>
2020-10-12 13:52:50 +0200

    Oprettede classer og planlage hvilket metoder der skal skrives

--- classes.js
+++ classes.js
@@ -1,16 +1,30 @@
 
 class User {
 
-    constructor(name, birthday, gender, location)
+    constructor(name, birthday, gender, location, email, password, uniqueUserID)
         {
       
         this.name = name
         this.birthday = birthday;
         this.gender = gender;
         this.location = location;
-    //Lav en like funiton
-        };
+        this.email = email;
+        this.password = password;
+        this.uniqueUserID = uniqueUserID;
+        }
+// Generer et userID som tæller op af alt efter hvornår man har oprettet en profil.
+var generateUserID = (function () {
+            var counter = 0;
+            return function () {counter += 1; return counter}
+          })();
 
+// When people like some one, the persones unique user ID will be addet to the array of people they like in the intrest class
+var like = function() {
+
+// tag userid fra den som man har liket og skriv ind i et array
+}
+
+    }
 class freeUser extends user{
     
             constructor(name, birthday, gender, location,){
