64101080f642c661434ee883ac9cb5587d8e772a
Mads Thorndahl <madsthorndahl@Madss-MBP.lan>
2020-10-13 00:20:05 +0200

    rettede i klasser og metoder, samt begyndte på at lave like funktionen og planlægge andre funktioner

--- Server.js
+++ Server.js
@@ -4,17 +4,11 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 
-// Funktion til at tælle op for hvert gang funktionen er kaldt
-var add = (function () {
-    var counter = 0;
-    return function () {counter += 1; return counter}
-  })();
-
 
 app.use(cors())
 //Bestemmer hvilket response der skal være når der er sendt et GET reques
 app.get('/',function(req,res){
-res.send("Hello world!" + " " + add())
+    res.send("Hello world!");
 });
 
 //Bestemmer hvilken port der skal lyttes på
--- classes.js
+++ classes.js
@@ -10,25 +10,22 @@ class User {
         this.location = location;
         this.email = email;
         this.password = password;
-        this.uniqueUserID = uniqueUserID;
-        }
-// Generer et userID som tæller op af alt efter hvornår man har oprettet en profil.
-var generateUserID = (function () {
-            var counter = 0;
-            return function () {counter += 1; return counter}
-          })();
-
-// When people like some one, the persones unique user ID will be addet to the array of people they like in the intrest class
-var like = function() {
-
-// tag userid fra den som man har liket og skriv ind i et array
-}
-
+        this.uniqueUserID = generateUserID;
+       
     }
+// Generer et userID som tæller op af alt efter hvornår man har oprettet en profil.
+generateUserID() {
+           
+    var counter = 0;
+    return function () {counter += 1; return counter}   
+};
+// tag userid fra den som man har liket og skriv ind i et array
+    
+
 class freeUser extends user{
     
-            constructor(name, birthday, gender, location,){
-            super(name, birthday, gender, location)
+            constructor(name, birthday, gender, location, email, password, uniqueUserID){
+            super(name, birthday, gender, location, email, password, uniqueUserID)
             //add like limit
             
         }}
@@ -36,8 +33,8 @@ class freeUser extends user{
 
 class paymenUser extends user{
     
-    constructor(name, birthday, gender, location,){
-    super(name, birthday, gender, location)
+    constructor(name, birthday, gender, location, email, password, uniqueUserID){
+    super(name, birthday, gender, location, email, password, uniqueUserID)
     //add super like eller sådan noget
     
 }}
@@ -53,12 +50,26 @@ class interst extends user {
     //hvis personen liker CRUD til en liste med alle folk som de liker
 
 }
+//laver et tomt array der indholde ID på alle dem man har liket
+var likedPersons = [];
 
 class match extends interst {
     constructor(name)
     super(name)
-    //metoder der sammenligner forskellige crud for forskellige persone og derved laver matches, og giver acces 
+    // metoder der sammenligner forskellige crud for forskellige persone og derved laver matches, og giver acces 
     // til en message function
 
 }
+
+// When people like some one, the persones unique user ID will be addet to the array of people they like in the intrest class
+like() {
+// Laver konstanter for fiktive like og disslike knapper
+Let likeBtn = document.getElementById("likeBtn");
+Let disslikeBtn = document.getElementById("disslikeBtn");
+// Laver en event listner der skal gøre noget alt efter om man liker eller dissliker
+likeBtn.addEventListener("click",()=>{
+//tilføj likede brugers uniqueId til et array 
+    likedPersons.push(userOnScreen.uniqueUserID) //lav en variabel der gør at den person der er på dislayet er = userOnScreen
+})
+};
 # GOP3
